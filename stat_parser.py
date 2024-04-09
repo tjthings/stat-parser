7f23f3cd564a14e93afd55a4bca180daf9dcf1ab
tjthings <s.tjthings@gmail.com>
2024-04-09 18:22:17 -0400

    Added Mojang API Request

--- stat_parser.py
+++ stat_parser.py
@@ -1,6 +1,7 @@
 import os
 import json
 import csv
+import requests
 from collections import defaultdict
 
 json_files = [pos_json for pos_json in os.listdir('stats_jsons') if pos_json.endswith('.json')]
@@ -14,7 +15,7 @@ for json_file in json_files:
 
 # https://minecraft.wiki/w/Statistics
 
-bac_columns = ['UUID', 'Time', 'Distance By Elytra', 'Fireworks Used', 'Deaths', 'Totems Used', 'Shulkers Opened', 'Diamond Pickaxe Uses', 'Diamond Shovel Uses', 'Diamond Axe Uses', 'Diamond Hoe Uses']
+bac_columns = ['UUID', 'Name', 'Time', 'Distance By Elytra', 'Fireworks Used', 'Deaths', 'Totems Used', 'Shulkers Opened', 'Diamond Pickaxe Uses', 'Diamond Shovel Uses', 'Diamond Axe Uses', 'Diamond Hoe Uses']
 bac_stats = [
     ["stats", "minecraft:custom", "minecraft:play_time"],
     ["stats", "minecraft:custom", "minecraft:aviate_one_cm"],
@@ -28,7 +29,7 @@ bac_stats = [
     ["stats", "minecraft:used", "minecraft:diamond_hoe"],
 ]
 
-melon_columns = ['UUID', 'Melons', 'Time', 'Dirt Mined', 'Grass Mined', 'Dirt Used', 'Grass Used', 'Moss Used', 'Distance By Elytra', 'Fireworks Used', 'Melon Seeds Used']
+melon_columns = ['UUID', 'Name', 'Melons', 'Time', 'Dirt Mined', 'Grass Mined', 'Dirt Used', 'Grass Used', 'Moss Used', 'Distance By Elytra', 'Fireworks Used', 'Melon Seeds Used']
 melon_stats = [
     ["stats", "minecraft:picked_up", "minecraft:melon_slice"],
     ["stats", "minecraft:custom", "minecraft:play_time"],
@@ -49,6 +50,11 @@ indiv_counts = defaultdict(list)
 count = 0
 for uuid in data_list:
     stats_file = data_list[uuid]
+
+    # Mojang API Request - UUID to IGN
+    r = requests.get('https://sessionserver.mojang.com/session/minecraft/profile/' + uuid)
+    indiv_counts[uuid].append(r.json()['name'])
+
     for stat in interesting_stats:
         if stat[0] in stats_file and stat[1] in stats_file[stat[0]] and stat[2] in stats_file[stat[0]][stat[1]]:
             stat_count = stats_file[stat[0]][stat[1]][stat[2]]
